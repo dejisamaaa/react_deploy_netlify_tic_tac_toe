f7f809ee3c73a12087aa6a3ac7a00608b99adf22
Ahmad Ayodeji Yusuf <witcherwildhunt354@gmail.com>
2026-09-05 00:00:04 +0100

    Remove unused import of useEffect from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Main from "./Main";
-import { useEffect } from 'react';
+
 function App() {
   return (
     <div className="App">
